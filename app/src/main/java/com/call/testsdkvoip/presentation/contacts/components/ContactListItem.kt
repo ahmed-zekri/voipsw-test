ebf242f5ff7b9fd8b1de5315f03616bbc99b90e8
ahmed-zekri <you@example.com>
2022-05-31 07:28:07 -0400

    delete unused packages

--- app/src/main/java/com/call/testsdkvoip/presentation/contacts/components/ContactListItem.kt
+++ app/src/main/java/com/call/testsdkvoip/presentation/contacts/components/ContactListItem.kt
@@ -2,14 +2,11 @@ package com.call.testsdkvoip.presentation.contacts.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
