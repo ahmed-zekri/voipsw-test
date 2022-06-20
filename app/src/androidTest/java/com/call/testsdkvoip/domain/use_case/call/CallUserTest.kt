cc3c641b65225f3159abcc84fb4fa85f1a27ee23
ahmed-zekri <ahmed.zekri.a@gmail.com>
2022-06-20 16:02:02 +0100

    call user unit testing

--- app/src/androidTest/java/com/call/testsdkvoip/domain/use_case/call/CallUserTest.kt
+++ app/src/androidTest/java/com/call/testsdkvoip/domain/use_case/call/CallUserTest.kt
@@ -1,13 +1,18 @@
 package com.call.testsdkvoip.domain.use_case.call
 
+import android.content.Context
 import androidx.test.filters.SmallTest
 import com.call.testsdkvoip.common.Resources
-import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
+import com.streamwide.smartms.lib.core.api.account.STWAccountManager
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.filterNot
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.last
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Rule
@@ -27,6 +32,10 @@ internal class CallUserTest {
     @Inject
     lateinit var callUser: CallUser
 
+    @Inject
+    @ApplicationContext
+    lateinit var context: Context
+
     @Before
     fun setUp() {
 
@@ -36,11 +45,24 @@ internal class CallUserTest {
     @Test
     fun testCallUser() {
 
-        runBlocking<Unit> {
-          launch {   callUser("21625306014").onEach {
-              Truth.assertThat(true)
+        runBlocking {
+            val it =
+                callUser("21625306014").filter { resource -> resource !is Resources.Loading }
+                    .first()
 
-          } }
+
+            if (!STWAccountManager.getInstance().isUserAuthenticated(context))
+
+                assertThat(
+                    !it.message!!.contains(
+                        "Exception"
+                    )
+                )
+            else
+                assertThat(
+                    it is Resources.Success
+                )
+
 
         }
     }
